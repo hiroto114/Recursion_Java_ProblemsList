181e2ec66ab663819f883f7c35bd9d1407f200cd
hiroto114 <147373496+hiroto114@users.noreply.github.com>
2024-09-23 10:48:26 +0900

    文字の集計を簡潔に

--- Recursion_Java_ProblemsList/src/Problem190.java
+++ Recursion_Java_ProblemsList/src/Problem190.java
@@ -8,15 +8,19 @@ import java.util.HashMap;
  * ただし、出現する回数が同じ場合は、大きい ASCII コードを持つ文字を優先してください。
  */
 public class Problem190 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(getAscii("abcabc") == 99); // 'c'のASCIIコード
+        System.out.println(getAscii("hello") == 108); // 'l'のASCIIコード
+        System.out.println(getAscii("aabbcc") == 99); // 'c'のASCIIコード
+        System.out.println(getAscii("aaaabbbbcccc") == 99); // 'c'のASCIIコード
+        System.out.println(getAscii("xyzxyzxyz") == 122); // 'z'のASCIIコード
+        System.out.println(getAscii("abcdeee") == 101); // 'e'のASCIIコード
+    }
 	public static int getAscii(String input) {
 		HashMap<Character, Integer> countInput = new HashMap<>();
 
 		for (int i = 0; i < input.length(); i++) {
-			if (countInput.get(input.charAt(i)) == null) {
-				countInput.put(input.charAt(i), 1);
-			} else {
-				countInput.replace(input.charAt(i), countInput.get(input.charAt(i)) + 1);
-			}
+			countInput.put(input.charAt(i),countInput.getOrDefault(input.charAt(i),0)+1);
 		}
 
 		Integer maxCount = Collections.max(countInput.values());
