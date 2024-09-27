74e3c85d6d104cfb95f9ac1cfbd48839d44f9818
0me6- <0me6-@192.168.1.9>
2024-09-28 03:17:27 +0900

    テストケース記載

--- Recursion_Java_ProblemsList/src/Problem289.java
+++ Recursion_Java_ProblemsList/src/Problem289.java
@@ -4,6 +4,15 @@
  * 下流側から順に並んでいる開発可能な地点の高さ一覧 height が与えられるので、
  * 最も多く水を収容できる開発開始地点と終了地点を求め、その時のダムの面積（高さ * 幅）を返す maxCapacity という関数を作成してください。
  * 
+ * print(maxCapacity([1, 8, 6, 2, 5, 4, 8, 3, 7])) # 49
+ * print(maxCapacity([5, 5, 5, 5, 5]))               # 20
+ * print(maxCapacity([3]))                            # 0
+ * print(maxCapacity([1, 1, 1, 1, 1, 1, 1]))        # 6
+ * print(maxCapacity([2, 1, 2]))                      # 2
+ * print(maxCapacity([4, 3, 2, 1, 4]))                # 16
+ * print(maxCapacity([0, 0, 0, 0]))                   # 0
+ * print(maxCapacity([1, 3, 2, 5, 25, 24, 5]))        # 24
+ * 
  */
 public class Problem289 {
     public static int maxCapacity(int[] height){
