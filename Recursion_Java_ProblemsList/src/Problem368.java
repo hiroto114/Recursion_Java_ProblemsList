e6702ed2ddea93954ae41ff9cc7611fd02d7862b
hiroto114 <147373496+hiroto114@users.noreply.github.com>
2024-09-29 10:33:01 +0900

    リファクタリング

--- Recursion_Java_ProblemsList/src/Problem368.java
+++ Recursion_Java_ProblemsList/src/Problem368.java
@@ -3,15 +3,11 @@
  */
 public class Problem368 {
     public static boolean recursiveIsPrime(int n){
-        if(n==1){
-            return false;
-        }else if(n==2||n==3){
-            return true;
-        }else{
-            for(int i=2;i<n;i++){
-                if(n%i==0)return false;
-            }
-            return true;
+        if(n==1) return false;
+        if(n==2||n==3) return true;
+        for(int i=2;i<n;i++){
+            if(n%i==0)return false;
         }
+        return true;
     }
 }
