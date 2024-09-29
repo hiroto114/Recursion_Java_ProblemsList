45d8093f398426e90ffd5a4914e7f4e7da68ea5c
hiroto114 <147373496+hiroto114@users.noreply.github.com>
2024-09-29 20:16:13 +0900

    リファクタリング

--- Recursion_Java_ProblemsList/src/Problem81.java
+++ Recursion_Java_ProblemsList/src/Problem81.java
@@ -2,9 +2,6 @@
 import java.util.ArrayList;
 import java.util.List;
 /*
- * Paul は数字を使ったゲームに参加しました。
- * 読み手が自然数を読み上げるので、参加者は床に落ちている数字から 2 つを選び、
- * それを掛け合わせた値が読み手の数字と一致したときに得点をもらえるルールです。
  * 床にばら撒かれている数字群を表す配列 numbers と読み手の数字 mark が与えられるので、
  * 掛け合わせたときに一致する 2 つの数字の位置を返す indexOfPairsForMark という関数を作成してください。
  * 複数組み合わせがある場合は最も若いインデックスが含まれているペアから順に 1 次配列で返してください。
@@ -20,10 +17,7 @@ public class Problem81 {
                 }
             }
         }
-        int[] stringArray = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            stringArray[i] = list.get(i);
-        }
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
